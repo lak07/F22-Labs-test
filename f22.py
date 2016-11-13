6965bd0f192e5af9868ebdef121f48596f45f93c
Lakshay Sharma <lavi9sar@gmail.com>
2016-11-13 20:42:08 +0530

    Update f22.py

--- f22.py
+++ f22.py
@@ -1,43 +1,47 @@
 import random
 import string
 import math
-
-
 def randomword():
 	length=random.randrange(3,20)	
 	return ''.join(random.choice(string.ascii_uppercase) for i in range(length))
 
-def check(code,guessed):
+def check(code,guessed,x):
+	d={}
 	correct=""
 	wrong=""
-	for x in guessed:
-		if x in code:
-			if code.index(x)==guessed.index(x):
-				correct+=x
-			else:
-				wrong+=x
+	print(code)
+	for x in range(0,len(guessed)):
+		for y in range(0,len(code)):
+			if code[y]==guessed[x]:
+				if y in d.keys():
+					continue				
+				if x==y:
+					correct+=guessed[x]
+					d[y]=code[y]
+				elif x!=y:
+					wrong+=guessed[x]					
+					d[y] = code[y]
+
 	if len(correct)==0:
 		correct="NO SUCH WORDS"
 	if len(wrong)==0:
-		wrong="NO SUCH WORDS"
-								
+		wrong="NO SUCH WORDS"				
 	print("Characters that are correct but in correct positions are:",correct)
 	print("Characters that are correct but in wrong positins are:",wrong)
-x=list(randomword())
-print("Length of generated code is:",len(x))
-ntr=round(math.sqrt(len(x)))
+generated=list(randomword())
+print("Length of generated code is:",len(generated))
+ntr=round(math.sqrt(len(generated)))
 print("Number of attempts allowed =",ntr)
 cnt=0
-print(x)
 flag=0
+print(generated)
 while(cnt!=ntr):
 	y=list(input().upper())
-	if y==x:
+	if y==generated:
 		print("You Guessed the code right!!")
 		flag=1
 		break
-	check(x,y)
+	check(generated,y,len(generated))
 	cnt+=1
 if flag!=1:
 	print("Sorry Better Luck Next time")	
-
