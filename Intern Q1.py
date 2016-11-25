dc414c88ad669d4d50b1c0964e4e8367d17df28f
Lakshay Sharma <lavi9sar@gmail.com>
2016-11-25 17:32:41 +0530

    Update Intern Q1.py

--- Intern Q1.py
+++ Intern Q1.py
@@ -1,3 +1,4 @@
+#This code is written in pyhton3 please use python3 to run this code.
 import random
 import string
 import math
@@ -5,11 +6,11 @@ def randomword():
 	length=random.randrange(3,20)	
 	return ''.join(random.choice(string.ascii_uppercase) for i in range(length))
 
-def check(code,guessed,x):
-	d={}# Dictionary is used to store words that are already checked.
+def check(code,guessed):
+	d={}
 	correct=""
 	wrong=""
-	#print(code)
+	print("generated code is :",''.join(code))
 	for x in range(0,len(guessed)):
 		for y in range(0,len(code)):
 			if code[y]==guessed[x]:
@@ -26,27 +27,41 @@ def check(code,guessed,x):
 				if x!=y:
 					wrong+=guessed[x]					
 					d[y] = code[y]
-
+	return correct,wrong
+def comp(code,maxx):
+	compgess=''.join(random.choice(string.ascii_uppercase) for i in range(maxx))
+	if compgess==code:
+		print("computer wins!")
+	(correct,wrong)=check(code,compgess)
+	
 	if len(correct)==0:
 		correct="NO SUCH WORDS"
 	if len(wrong)==0:
 		wrong="NO SUCH WORDS"				
-	print("Characters that are correct and in correct positions are:",correct)
-	print("Characters that are correct but in wrong positins are:",wrong)
-generated=list(randomword())#string generated randomly by code
+	print("Characters that are correctly guessed by computer and are in correct positions are:",correct)
+	print("Characters that are correctly guessed by computer but are in wrong positins are:",wrong)
+generated=list(randomword())
 print("Length of generated code is:",len(generated))
-ntr=round(math.sqrt(len(generated)))#no. of tries
+ntr=round(math.sqrt(len(generated)))
 print("Number of attempts allowed =",ntr)
 cnt=0
 flag=0
-#print(generated)
+print("generated code is :",''.join(generated))
 while(cnt!=ntr):
-	y=list(input().upper())#input string from user
+	y=list(input().upper())
 	if y==generated:
 		print("You Guessed the code right!!")
 		flag=1
 		break
-	check(generated,y,len(generated))
+	(correct,wrong)=check(generated,y)
+	if len(correct)==0:
+		correct="NO SUCH WORDS"
+	if len(wrong)==0:
+		wrong="NO SUCH WORDS"				
+	print("Characters that are correct but in correct positions are:",correct)
+	print("Characters that are correct but in wrong positins are:",wrong)
+	print("Computer player stats are:--------")
+	comp(generated,len(generated))
 	cnt+=1
 if flag!=1:
 	print("Sorry Better Luck Next time")	
